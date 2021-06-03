e4641053a2279fac62f80bc76ab7339992ed2f14
Mihai-Stefan <mihaiste@gmail.com>
2021-06-04 01:33:26 +0300

    SendMessages .cs and SendEmailController implemented

---
+++ Controllers/SendEmailsController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using cardata_api_v3.Models;
+
+
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace cardata_api_v3.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SendEmailsController : ControllerBase
+    {
+
+        private readonly CarContext _context;
+
+        public SendEmailsController(CarContext context)
+        {
+            _context = context;
+            foreach (var item in context.Employees)
+            {
+                if(item.AssignedCar == null)
+                {
+                    string email = "mihaiste@gmail.com";
+                    SendMessages.SendMail(item.FirstName + " " + item.LastName, email).Wait();
+                }
+            }
+        }
+
+        public void Get()
+        {
+            Response.Redirect("https://localhost:44391/admin");
+        }
+
+
+    }
+}
---
+++ Controllers/SendMessages.cs
@@ -0,0 +1,38 @@
+﻿using System.Threading.Tasks;
+using SendGrid;
+using SendGrid.Helpers.Mail;
+
+
+namespace cardata_api_v3.Controllers
+{
+    class SendMessages
+    {
+        public static async Task SendMail(string name, string email)
+        {
+            // daca memorez api_key intr-o variabila de sistem
+            //var apiKey = Environment.GetEnvironmentVariable("SENDGRID_API_KEY");
+            var apiKey = "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";
+            var client = new SendGridClient(apiKey);
+            var from = new EmailAddress("mihaiste@gmail.com");
+            var to = new EmailAddress(email);
+            var subject = "Message send with SendGrid from C# app to: " + name;
+            var plainTextContent = "This is the plain message text.";
+            var htmlContent = "<b> And this is HTML content of the message</b>" +
+                "<h3>Second mail</h3>";
+            //acum creem mailul, cu ajutorul constructorului:
+            var msg = MailHelper.CreateSingleEmail(
+                from,
+                to,
+                subject,
+                plainTextContent,
+                htmlContent
+                );
+
+            //send email, by calling client:
+            var response = await client.SendEmailAsync(msg);
+            var x = 20;
+
+
+        }
+    }
+}
