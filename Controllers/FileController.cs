fadc04b210260c495e18b22dfaab6d696e2bf9c3
Mihai-Stefan <mihaiste@gmail.com>
2021-06-17 13:05:07 +0300

    Added FileController.cs and FileModel.cs

---
+++ Controllers/FileController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.IO;
+using cardata_api_v3.Models;
+using Microsoft.AspNetCore.Http;
+
+namespace cardata_api_v3.Controllers
+{
+    [Route("api/file")]
+    [ApiController]
+    public class FileController : ControllerBase
+    {
+
+        [HttpPost]
+        public ActionResult Post([FromForm] FileModel file)
+        {
+            try
+            {
+                string path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", file.FileName);
+
+                using (Stream stream = new FileStream(path, FileMode.Create))
+                {
+                    file.FormFile.CopyTo(stream);
+                }
+
+                return StatusCode(StatusCodes.Status201Created);
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
+        }
+    }
+}
