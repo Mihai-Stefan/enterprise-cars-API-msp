10cfa9fa090fcd7068297a90cab4bff76a0b8884
Mihai-Stefan <mihaiste@gmail.com>
2021-06-17 16:30:46 +0300

    Alex Modifies

--- Controllers/FileController.cs
+++ Controllers/FileController.cs
@@ -3,6 +3,7 @@ using System;
 using System.IO;
 using cardata_api_v3.Models;
 using Microsoft.AspNetCore.Http;
+using System.Collections.Generic;
 
 namespace cardata_api_v3.Controllers
 {
@@ -11,9 +12,35 @@ namespace cardata_api_v3.Controllers
     public class FileController : ControllerBase
     {
 
+        private readonly CarContext _context;
+
+        public FileController(CarContext context)
+        {
+            _context = context;
+        }
+
         [HttpPost]
         public ActionResult Post([FromForm] FileModel file)
         {
+            Stream csvStream = new MemoryStream();
+            file.FormFile.CopyTo(csvStream);
+            //var reader = new StreamReader(csvStream);
+            
+                using (var reader = new StreamReader(csvStream))
+                {
+                    List<string> listA = new List<string>();
+                    List<string> listB = new List<string>();
+                    while (!reader.EndOfStream)
+                    {
+                        var line = reader.ReadLine();
+                        var values = line.Split(';');
+
+                        listA.Add(values[0]);
+                        listB.Add(values[1]);
+                    }
+                }
+            
+
             try
             {
                 string path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", file.FileName);
